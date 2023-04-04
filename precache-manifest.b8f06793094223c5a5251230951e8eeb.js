5c26851c0074747fea6761114acb0a32ee023402
Chris Cline <115193125+ccline29@users.noreply.github.com>
2023-04-03 19:28:58 -0500

    Updates

---
+++ precache-manifest.b8f06793094223c5a5251230951e8eeb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a63e183f9d009204f45654bdc336b3",
+    "url": "/ccline29/React_My_Portfolio/index.html"
+  },
+  {
+    "revision": "235172afed28f80fd10e",
+    "url": "/ccline29/React_My_Portfolio/static/css/main.4ba628e4.chunk.css"
+  },
+  {
+    "revision": "4f6f4d7b36e50ad227c3",
+    "url": "/ccline29/React_My_Portfolio/static/js/2.f42686ba.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/ccline29/React_My_Portfolio/static/js/2.f42686ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235172afed28f80fd10e",
+    "url": "/ccline29/React_My_Portfolio/static/js/main.a5f293b3.chunk.js"
+  },
+  {
+    "revision": "82020b04e771d3b1dfde",
+    "url": "/ccline29/React_My_Portfolio/static/js/runtime-main.62b80cf8.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/ccline29/React_My_Portfolio/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "b9d7d448a09d6aacb45d21aab2c6fd53",
+    "url": "/ccline29/React_My_Portfolio/static/media/your photo.b9d7d448.png"
+  }
+]);
